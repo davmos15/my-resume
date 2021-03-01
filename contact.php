0032bf37ebe53117e2e3cadb449ea8d616a7425a
davmos15 <64386509+davmos15@users.noreply.github.com>
2021-03-01 20:13:55 +1100

    Contact PHP #2

--- contact.php
+++ contact.php
@@ -1,22 +1,19 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
 
-    $email_from = 'Submission@NadavsResume.com';
+// an email address that will be in the From field of the email.
+$from = 'Demo contact form <demo@domain.com>';
 
-    $email_subject = "New Form Submission";
+// an email address that will receive the email with the output of the form
+$sendTo = 'Demo contact form <moskownadav@domain.com>';
 
-    $email_body = "User Name: $name.\n".
-                    "User Message: $message.\n";
+// subject of the email
+$subject = 'New message from contact form';
 
-    $to = "moskownadav@gmail.com";
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'message' => 'Message'); 
 
-    $headers = "From: $email_from \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: contact.html")
-
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
 ?>
