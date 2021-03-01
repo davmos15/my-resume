051e59216da380f5f05753956afdb9596e197183
davmos15 <64386509+davmos15@users.noreply.github.com>
2021-03-01 20:20:02 +1100

    Update #3

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Demo contact form <forms@nadavsresume.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <moskownadav@domain.com>';
@@ -11,7 +11,7 @@ $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'message' => 'Message'); 
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
